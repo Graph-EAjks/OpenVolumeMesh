7745da1d4cf65785fa2e309e3e98bdc956500267
vn20x841 <valentin.nigolian@inf.unibe.ch>
2020-07-16 13:45:16 +0200

    set-up laplacian computation files

---
+++ src/OpenVolumeMesh/Mesh/PolyhedralMeshLaplacian.hh
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace OpenVolumeMesh{
+
+class PolyhedralMeshLaplacian
+{
+public:
+
+};
+
+}
---
+++ src/Unittests/unittests_laplacians.cc
@@ -0,0 +1,11 @@
+#include "unittests_common.hh"
+#include <OpenVolumeMesh/Mesh/PolyhedralMeshLaplacian.hh>
+
+
+using namespace OpenVolumeMesh;
+
+
+TEST_F(PolyhedralMeshBase, CreateUniformLaplacian){
+
+    PolyhedralMeshLaplacian laplacian;
+}
