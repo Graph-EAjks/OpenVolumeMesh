0cdf16c841a6e5e55409eddedea566763a891dc1
vn20x841 <valentin.nigolian@inf.unibe.ch>
2020-07-17 12:43:47 +0200

    implemented uniform laplacian

--- src/OpenVolumeMesh/Mesh/PolyhedralMeshLaplacians.hh
+++ src/OpenVolumeMesh/Mesh/PolyhedralMeshLaplacians.hh
@@ -11,14 +11,34 @@ public:
 
     PolyhedralMeshLaplacian(_polyhedral_mesh& mesh) : mesh_(mesh) {}
 
-    double operator[](const EdgeHandle& edge){
-        return 1;
-    }
-
-
     double operator[](const EdgeHandle& edge) const{
         return 1;
     }
+
+    double operator[](const HalfEdgeHandle& half_edge) const {
+        return 1;
+    }
+
+
+
+    Vec3d operator[](const VertexHandle& vertex) const{
+
+        Vec3d weighted_average({0,0,0});
+        double weight_sum(0);
+
+        auto voh_it = mesh_.voh_iter(vertex);
+        while(voh_it.valid()){
+
+            double he_weight = (*this)[*voh_it];
+
+            weighted_average += he_weight * mesh_.vertex(mesh_.to_vertex_handle(*voh_it));
+            weight_sum += he_weight;
+
+            voh_it++;
+        }
+        return weighted_average / weight_sum;
+    }
+
 
 private:
 
--- src/Unittests/unittests_laplacians.cc
+++ src/Unittests/unittests_laplacians.cc
@@ -9,6 +9,11 @@ class PolyhedralMeshLaplacianTest : public PolyhedralMeshBase{
 
 public:
 
+    //generate a basic mesh for further tests
+    void SetUp() override{
+        generatePolyhedralMesh(mesh_);
+    }
+
 private:
 
 };
@@ -24,9 +29,37 @@ TEST_F(PolyhedralMeshLaplacianTest, CreateLaplacian){
 TEST_F(PolyhedralMeshLaplacianTest, EdgeUniformLaplacianEqualsOneEverywhere){
 
     PolyhedralMeshLaplacian<PolyhedralMesh> laplacian(mesh_);
+    ASSERT_GT(mesh_.e_vertices(), 0);
 
     for(auto e_it = mesh_.edges_begin(); e_it != mesh_.edges_end(); e_it++){
         ASSERT_EQ(laplacian[*e_it], 1);
+
+    }
+
+}
+
+
+
+TEST_F(PolyhedralMeshLaplacianTest, VertexUniformLaplacianEqualsAverageOfNeighborsPositions){
+
+    PolyhedralMeshLaplacian<PolyhedralMesh> laplacian(mesh_);
+    ASSERT_GT(mesh_.n_vertices(), 0);
+
+    for(auto v_it = mesh_.vertices_begin(); v_it != mesh_.vertices_end(); v_it++){
+
+        Vec3d average_position({0,0,0});
+        double vertex_degree(0);
+        auto vv_it = mesh_.vv_iter(*v_it);
+        while(vv_it.valid()){
+            average_position += mesh_.vertex(*vv_it);
+            vertex_degree++;
+
+            vv_it++;
+        }
+        average_position /= vertex_degree;
+
+
+        ASSERT_EQ(laplacian[*v_it], average_position);
 
     }
 
