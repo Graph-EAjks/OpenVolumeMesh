3aa1996284283ab0a686a684d4e4b3ef864aaed9
Martin Heistermann <git@mheistermann.de>
2021-11-20 11:09:04 +0100

    Add local topology helper classes.

---
+++ src/OpenVolumeMesh/Topology/TetTopology.cc
@@ -0,0 +1,93 @@
+#include <OpenVolumeMesh/Topology/TetTopology.hh>
+
+namespace OpenVolumeMesh {
+
+static HFH find_halfface(TetrahedralMeshTopologyKernel const &_mesh, CH _ch, VH _vh) {
+    for (const auto hfh: _mesh.cell_halffaces(_ch)) {
+        for (const auto vh: _mesh.halfface_vertices(hfh)) {
+            if (vh == _vh)
+                return hfh;
+        }
+    }
+    assert(false);
+    return {};
+}
+
+
+TetTopology::TetTopology(const TetrahedralMeshTopologyKernel &mesh,
+                         CH _ch,
+                         HFH _abc,
+                         VH _a)
+{
+    auto abc_he_it = mesh.hfhe_iter(_abc, 2);
+    if (_a.is_valid()) {
+        while(mesh.from_vertex_handle(*abc_he_it) != _a) {
+            ++abc_he_it;
+        }
+    }
+    heh_[AB] = *abc_he_it++;
+    heh_[BC] = *abc_he_it++;
+    heh_[CA] = *abc_he_it;
+
+    vh_[A] = mesh.from_vertex_handle(ab());
+    vh_[B] = mesh.from_vertex_handle(bc());
+    vh_[C] = mesh.from_vertex_handle(ca());
+
+    assert(b() == mesh.to_vertex_handle(ab()));
+    assert(c() == mesh.to_vertex_handle(bc()));
+    assert(a() == mesh.to_vertex_handle(ca()));
+
+    hfh_[ABC] = _abc;
+    for (const auto hfh: mesh.cell_halffaces(_ch)) {
+        if (hfh == _abc) continue;
+        auto heh_it = mesh.hfhe_iter(hfh, 2);
+        while (heh_it) {
+            auto heh = *(heh_it++);
+            if (heh == ba()) {
+                hfh_[BAD] = hfh;
+                heh_[AD] = *heh_it;
+                break;
+            }
+            if (heh == cb()) {
+                hfh_[CBD] = hfh;
+                heh_[BD] = *heh_it;
+                break;
+            }
+            if (heh == ac()) {
+                hfh_[ACD] = hfh;
+                heh_[CD] = *heh_it;
+                break;
+            }
+        }
+    }
+    vh_[D] = mesh.to_vertex_handle(ad());
+
+    assert(d() != a());
+    assert(d() != b());
+    assert(d() != c());
+
+    assert(d() == mesh.to_vertex_handle(ad()));
+    assert(d() == mesh.to_vertex_handle(bd()));
+    assert(d() == mesh.to_vertex_handle(cd()));
+
+    assert(a() == mesh.from_vertex_handle(ad()));
+    assert(b() == mesh.from_vertex_handle(bd()));
+    assert(c() == mesh.from_vertex_handle(cd()));
+}
+
+
+TetTopology::TetTopology(TetrahedralMeshTopologyKernel const &_mesh,
+        HFH _abc, VH _a)
+    : TetTopology(_mesh, _mesh.incident_cell(_abc), _abc, _a)
+{}
+
+TetTopology::TetTopology(const TetrahedralMeshTopologyKernel &_mesh,
+                         CH _ch, VH _a)
+    : TetTopology(_mesh, _ch, find_halfface(_mesh, _ch, _a), _a)
+{}
+
+TetTopology::TetTopology(const TetrahedralMeshTopologyKernel &_mesh, CH _ch)
+    : TetTopology(_mesh, *_mesh.chf_iter(_ch))
+{}
+
+} // namespace OpenVolumeMesh
---
+++ src/OpenVolumeMesh/Topology/TetTopology.hh
@@ -0,0 +1,126 @@
+#pragma once
+
+#include <OpenVolumeMesh/Config/Export.hh>
+#include <OpenVolumeMesh/Core/Handles.hh>
+#include <OpenVolumeMesh/Mesh/TetrahedralMeshTopologyKernel.hh>
+
+namespace OpenVolumeMesh {
+
+
+class OVM_EXPORT TetTopology {
+public:
+    enum VertexLabel : uint8_t {
+        A, B, C, D
+    };
+    enum HalfEdgeLabel : uint8_t {
+        // halfedge i+3 is vertex-disjunct with halfedge i (\in [0..2])
+        AB, BC, CA,
+        CD, AD, BD,
+    };
+    enum OppHalfEdgeLabel : uint8_t {
+        BA = HalfEdgeLabel::AB,
+        CB = HalfEdgeLabel::BC,
+        AC = HalfEdgeLabel::CA,
+        DC = HalfEdgeLabel::CD,
+        DA = HalfEdgeLabel::AD,
+        DB = HalfEdgeLabel::BD,
+    };
+    enum HalfFaceLabel : uint8_t {
+        BDC = 0, DCB = 0, CBD = 0, OppA = 0,
+        ACD = 1, CDA = 1, DAC = 1, OppB = 1,
+        BAD = 2, ADB = 2, DBA = 2, OppC = 2,
+        ABC = 3, BCA = 3, CAB = 3, OppD = 3,
+    };
+
+    TetTopology(TetrahedralMeshTopologyKernel const &mesh, CH ch, HFH abc, VH a=VH());
+    TetTopology(TetrahedralMeshTopologyKernel const &mesh, HFH abc, VH a=VH());
+    TetTopology(TetrahedralMeshTopologyKernel const &mesh, CH ch, VH a);
+    TetTopology(TetrahedralMeshTopologyKernel const &mesh, CH ch);
+
+    const auto &halfface_handles() const {return hfh_;}
+
+    template<VertexLabel I>
+    constexpr VH vh() const {return vh_[I];}
+    constexpr VH vh(VertexLabel i) const {return vh_[i];}
+    constexpr VH vh(int i) const {return vh_[i];}
+
+    template<HalfEdgeLabel I>
+    constexpr HEH heh() const {return heh_[I];}
+    constexpr HEH heh(HalfEdgeLabel i) const {return heh_[i];}
+
+    template<OppHalfEdgeLabel I>
+    constexpr HEH heh() const {return heh_[I].opposite_handle();}
+    constexpr HEH heh(OppHalfEdgeLabel i) const {return heh_[i].opposite_handle();}
+
+    /// hfh<i>() is opposite to the vertex obtained by corner(i)
+    template<HalfFaceLabel I>
+    constexpr HFH hfh() const {return hfh_[I];}
+    constexpr HFH hfh(HalfFaceLabel i) const {return hfh_[i];}
+    constexpr HFH hfh(int i) const {return hfh_[i];}
+
+    template<unsigned int X, unsigned int Y>
+    constexpr HEH heh() const {
+        static_assert(X<4);
+        static_assert(Y<4);
+        static_assert(X!=Y);
+        if constexpr (X == 0 && Y == 1) return ab();
+        if constexpr (X == 0 && Y == 2) return ac();
+        if constexpr (X == 0 && Y == 3) return ad();
+
+        if constexpr (X == 1 && Y == 0) return ba();
+        if constexpr (X == 1 && Y == 2) return bc();
+        if constexpr (X == 1 && Y == 3) return bd();
+
+        if constexpr (X == 2 && Y == 0) return ca();
+        if constexpr (X == 2 && Y == 1) return cb();
+        if constexpr (X == 2 && Y == 3) return cd();
+
+        if constexpr (X == 3 && Y == 0) return da();
+        if constexpr (X == 3 && Y == 1) return db();
+        if constexpr (X == 3 && Y == 2) return dc();
+    }
+
+    constexpr VH a() const {return vh<A>();}
+    constexpr VH b() const {return vh<B>();}
+    constexpr VH c() const {return vh<C>();}
+    constexpr VH d() const {return vh<D>();}
+
+    // half-edges
+    constexpr HEH ab() const {return heh<AB>();}
+    constexpr HEH ca() const {return heh<CA>();}
+    constexpr HEH ad() const {return heh<AD>();}
+    constexpr HEH bc() const {return heh<BC>();}
+    constexpr HEH bd() const {return heh<BD>();}
+    constexpr HEH cd() const {return heh<CD>();}
+
+    constexpr HEH ac() const {return ca().opposite_handle();}
+    constexpr HEH ba() const {return ab().opposite_handle();}
+    constexpr HEH da() const {return ad().opposite_handle();}
+    constexpr HEH dc() const {return cd().opposite_handle();}
+    constexpr HEH db() const {return bd().opposite_handle();}
+    constexpr HEH cb() const {return bc().opposite_handle();}
+
+    // half-faces with all permutations
+    constexpr HFH bdc() const {return hfh_[0];}
+    constexpr HFH dcb() const {return hfh_[0];}
+    constexpr HFH cbd() const {return hfh_[0];}
+
+    constexpr HFH acd() const {return hfh_[1];}
+    constexpr HFH cda() const {return hfh_[1];}
+    constexpr HFH dac() const {return hfh_[1];}
+
+    constexpr HFH bad() const {return hfh_[2];}
+    constexpr HFH adb() const {return hfh_[2];}
+    constexpr HFH dba() const {return hfh_[2];}
+
+    constexpr HFH abc() const {return hfh_[3];}
+    constexpr HFH bca() const {return hfh_[3];}
+    constexpr HFH cab() const {return hfh_[3];}
+
+private:
+    std::array<VH, 4> vh_;
+    std::array<HEH, 6> heh_;
+    std::array<HFH, 4> hfh_;
+};
+
+} // namespace OpenVolumeMesh
