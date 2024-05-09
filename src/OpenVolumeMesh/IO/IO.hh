6b0899539df457c90a11db1bff9f92ce605033d4
Martin Heistermann <git@mheistermann.de>
2024-05-09 20:59:57 +0200

    IO read_file refactor, new read_mesh()

--- src/OpenVolumeMesh/IO/IO.hh
+++ src/OpenVolumeMesh/IO/IO.hh
@@ -2,31 +2,43 @@
 
 #include <OpenVolumeMesh/IO/ovmb_read.hh>
 #include <OpenVolumeMesh/FileManager/FileManager.hh>
+#include <filesystem>
 #include <stdexcept>
 
 namespace OpenVolumeMesh::IO {
 
 /// Read a mesh in ovm or ovmb file format depending on the file ending.
 template<typename MeshT>
+[[deprecated("Prefer read_mesh()")]]
 bool read_file(std::string const&_filename, MeshT &_mesh,
                bool _topo_check = true, bool _bottom_up_incidences = true)
 {
-    if (_filename.size() < 4) {
-        throw std::runtime_error("Filename too short: must end in .ovm or .ovmb");
-    }
-    std::string last4 = _filename.substr(_filename.size() - 4, 4);
-    if (last4 == ".ovm") {
+    ReadOptions options;
+    options.topology_check = _topo_check;
+    options.bottom_up_incidences = _bottom_up_incidences;
+    auto res = read_mesh(_filename, _mesh, options);
+    return res == ReadResult::Ok;
+}
+
+template<typename MeshT>
+ReadResult read_mesh(
+        std::string const&_filename,
+        MeshT &_mesh,
+        ReadOptions const&options)
+{
+    const auto path = std::filesystem::path(_filename);
+    const std::string ext = path.extension();
+
+    if (ext == "ovmb") {
+        return ovmb_read(_filename.c_str(), _mesh, options);
+    } else if (ext == "ovm") {
         FileManager file_manager;
-        return file_manager.readFile(_filename, _mesh, _topo_check, _bottom_up_incidences);
-    } else if (last4 == "ovmb") {
-        ReadOptions options;
-        options.topology_check = _topo_check;
-        options.bottom_up_incidences = _bottom_up_incidences;
-        auto result = ovmb_read(_filename.c_str(), _mesh, options);
-        return result == ReadResult::Ok;
+        auto ok = file_manager.readFile(_filename, _mesh,
+                options.topology_check,
+                options.bottom_up_incidences);
+        return ok ? ReadResult::Ok : ReadResult::OtherError;
     } else {
-        throw std::runtime_error("Unknown file extension: must end in .ovm or .ovmb");
-        return false;
+        return ReadResult::UnknownExtension;
     }
 }
 
