ef579256489d1b7188ef681734e00a1e49610c63
vn20x841 <valentin.nigolian@inf.unibe.ch>
2025-01-29 13:29:49 +0100

    fixed iterators increment for topological link stuff

--- src/OpenVolumeMesh/Core/detail/TopologicalLinkT_impl.hh
+++ src/OpenVolumeMesh/Core/detail/TopologicalLinkT_impl.hh
@@ -12,17 +12,17 @@ namespace OpenVolumeMesh {
         FaceSet faces;
 
         // all neighbouring vertices are in the link
-        for (auto v = mesh.vv_iter(vertex); v.valid(); ++v) {
+        for (auto v = mesh.vv_iter(vertex); v.valid(); v++) {
             vertices.insert(*v);
         }
 
         // all edges, which are opposite of the vertex in a face are in the link and all faces which are opposite of
         // the vertex in a tet including all their edges are in the link.
-        for (auto c = mesh.vc_iter(vertex); c.valid(); ++c) {
+        for (auto c = mesh.vc_iter(vertex); c.valid(); c++) {
 
-            for (auto hf = mesh.chf_iter(*c); hf.valid(); ++hf) {
+            for (auto hf = mesh.chf_iter(*c); hf.valid(); hf++) {
                 bool found(false);
-                for (auto v = mesh.hfv_iter(*hf); v.valid(); ++v) {
+                for (auto v = mesh.hfv_iter(*hf); v.valid(); v++) {
                     if (*v == vertex) {
                         found = true;
                         break;
@@ -32,7 +32,7 @@ namespace OpenVolumeMesh {
                 // the face does not contain the original vertex, so it is on the opposite side of the tet
                 if (!found) {
                     faces.insert(mesh.face_handle(*hf));
-                    for (auto e = mesh.hfe_iter(*hf); e.valid(); ++e) {
+                    for (auto e = mesh.hfe_iter(*hf); e.valid(); e++) {
                         edges.insert(*e); // an edge might be inserted more than once, but that's ok as edges is a set
                     }
                 }
@@ -40,8 +40,8 @@ namespace OpenVolumeMesh {
         }
 
         // also include edges which are not part of an incident cell of the vertex, but only of an incident face
-        for (auto f = mesh.vf_iter(vertex); f.valid(); ++f) {
-            for (auto e = mesh.fe_iter(*f); e.valid(); ++e) {
+        for (auto f = mesh.vf_iter(vertex); f.valid(); f++) {
+            for (auto e = mesh.fe_iter(*f); e.valid(); e++) {
                 bool found(false);
                 if (mesh.edge(*e).to_vertex() == vertex ||
                     mesh.edge(*e).from_vertex() == vertex) {
@@ -68,9 +68,9 @@ namespace OpenVolumeMesh {
         auto to_vertex = mesh.edge(edge).to_vertex();
 
         // all edges opposite of a tet including their end vertices are in the link
-        for (auto c = mesh.ec_iter(edge); c.valid(); ++c) {
+        for (auto c = mesh.ec_iter(edge); c.valid(); c++) {
 
-            for (auto e = mesh.ce_iter(*c); e.valid(); ++e) {
+            for (auto e = mesh.ce_iter(*c); e.valid(); e++) {
 
                 auto other_from_vertex = mesh.edge(*e).from_vertex();
                 auto other_to_vertex = mesh.edge(*e).to_vertex();
@@ -89,15 +89,14 @@ namespace OpenVolumeMesh {
         }
 
         // also check for neighbouring vertices not incident to the same cell
-        for (auto f = mesh.ef_iter(edge); f.valid(); ++f) {
-            for (auto v = mesh.fv_iter(*f); ++v;) {
+        for (auto f = mesh.ef_iter(edge); f.valid(); f++) {
+            for (auto v = mesh.fv_iter(*f); v++;) {
                 if (*v != mesh.edge(edge).to_vertex() &&
                     *v != mesh.edge(edge).from_vertex()) {
                     vertices.insert(*v);
                 }
             }
         }
-
 
         return {vertices, edges, {}, {}};
 
@@ -125,7 +124,8 @@ namespace OpenVolumeMesh {
 
         auto vertex_link_intersection = link(mesh, from_vertex).intersection(link(mesh, to_vertex));
 
-        //TODO: comment why this holds
+        //the link of an edge cannot contain any face.
+        // Therefore if the intersection's link contains a face, it cannot be equal to the edge's link
         if (vertex_link_intersection.faces().size()) {
             return false;
         }
