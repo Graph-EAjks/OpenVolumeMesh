b08765777356a67ee9a282dc99ffca940449bd13
Martin Heistermann <git@mheistermann.de>
2026-02-03 19:50:56 +0100

    Eigen support: specialize vector_dim for use in GeometryKernel.

--- src/OpenVolumeMesh/IO/PropertyCodecsEigen.hh
+++ src/OpenVolumeMesh/IO/PropertyCodecsEigen.hh
@@ -8,6 +8,17 @@
 #include <Eigen/Core>
 
 
+namespace OpenVolumeMesh::Geometry {
+template<typename _Scalar, int _Cols>
+struct vector_dim<Eigen::Matrix<_Scalar, 1, _Cols>> {
+    constexpr static int dim = _Cols;
+};
+template<typename _Scalar, int _Rows>
+struct vector_dim<Eigen::Matrix<_Scalar, _Rows, 1>> {
+    constexpr static int dim = _Rows;
+};
+}
+
 namespace OpenVolumeMesh::IO::Codecs {
 
 template<typename _Scalar, int _Rows, int _Cols>
